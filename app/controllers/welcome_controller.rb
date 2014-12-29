e08896e55ee66bb513a0e024a7b71754c9c13df5
psyho99 <psyho99@gmail.com>
2014-12-29 21:36:12 +0100

    tesciki

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,7 @@
 class WelcomeController < ApplicationController
+	
+	def dupa
+		@time = Time.now
+
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'say/hello'
+
+  get 'say/goodbye'
+
+  get 'welcome/dupa'
+
 	root 'welcome#index'
 end
